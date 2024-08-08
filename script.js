b11ea8e58843cc16b29811a10bb6199c55f7aeea
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-08-08 08:06:35 -0500

    project

---
+++ script.js
@@ -0,0 +1,38 @@
+/*
+Define Variables for Coins:
+Create variables to store the number of each type of coin: pennies, nickels, dimes, and quarters.
+*/
+let currencyValue = [.01, 0.5, .10, .25];
+let coinsPennies = 100;
+let coinsNickels = 80;
+let coinsDimes = 200;
+let coinsQaurters = 80;
+let total;
+/*
+Calculate Total Value of Coins:
+Write code to calculate the total value of each type of coin in dollars.
+Calculate the combined total value of all coins.
+*/
+
+
+
+
+/*
+Sort Coins into Wrappers:
+Use the standard capacity for each type of coin wrapper:
+Pennies: 50 coins per wrapper
+Nickels: 40 coins per wrapper
+Dimes: 50 coins per wrapper
+Quarters: 40 coins per wrapper
+Calculate the number of wrappers needed for each type of coin.
+Calculate the remaining coins that do not fill a wrapper completely.
+*/
+
+
+/*
+Output Results:
+Use console.log to display the number of wrappers needed for each type of coin.
+Use console.log to display the total amount in dollars.
+*/
+
+
