49de6bc402a61e44fc5fdf67b6633863b4c24cd1
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-08-08 09:23:03 -0500

    values to two decimal places

--- script.js
+++ script.js
@@ -2,21 +2,50 @@
 Define Variables for Coins:
 Create variables to store the number of each type of coin: pennies, nickels, dimes, and quarters.
 */
-let currencyValue = [.01, 0.5, .10, .25];
-let coinsPennies = 100;
-let coinsNickels = 80;
-let coinsDimes = 200;
-let coinsQaurters = 80;
-let total;
+let currencyValue = [.01, 0.05, .10, .25];
+let coins = [1500, 601, 411, 170];
+let type = ['pennies', 'nickels', 'dimes', 'quarters'];
+let myCoins=[];
+let total = 0;
 /*
 Calculate Total Value of Coins:
 Write code to calculate the total value of each type of coin in dollars.
 Calculate the combined total value of all coins.
 */
+let pennies = currencyValue[0] * coins[0];
+//
+//pennies = Math.round(pennies*100/100);
 
+myCoins.push(pennies);
+pennies = pennies.toFixed(2);
+console.log('$'+ pennies + ' in ' + type[0])
 
+let nickels = currencyValue[1] * coins[1];
+//nickels = Math.round(nickels*100)/100;
+myCoins.push(nickels);
+nickels = nickels.toFixed(2);
+console.log('$'+ nickels + ' in ' + type[1])
 
+let dimes = currencyValue[2] * coins[2];
+//dimes = Math.round(dimes*100)/100;
+myCoins.push(dimes);
+dimes = dimes.toFixed(2);
+console.log('$'+ dimes + ' in ' + type[2])
 
+let quarters = currencyValue[3] * coins[3];
+//quarters = Math.round(quarters*100)/100;
+myCoins.push(quarters);
+quarters = quarters.toFixed(2);
+console.log('$'+ quarters + ' in ' + type[3])
+
+console.log(myCoins)
+
+for(let i=0; i<myCoins.length; i++){
+ total += myCoins[i];
+}
+total = total.toFixed(2);
+
+console.log('$' + total + ' in total')
 /*
 Sort Coins into Wrappers:
 Use the standard capacity for each type of coin wrapper:
